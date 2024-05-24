7dacfcb6b1247e219a3af1e37b32507f78363fcb
Adrian72808 <168490652+Adrian72808@users.noreply.github.com>
2024-05-25 00:40:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Zadanie4_lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(23, 26);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Wybierz";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(23, 70);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(23, 114);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(120, 28);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(336, 20);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 173);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "RSA File Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie4_lab3
+{
+    public partial class Form1 : Form
+    {
+        private RSACryptoServiceProvider rsaProvider;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rsaProvider = new RSACryptoServiceProvider();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFilePath.Text))
+            {
+                try
+                {
+                    byte[] fileBytes = File.ReadAllBytes(txtFilePath.Text);
+                    byte[] encryptedBytes = rsaProvider.Encrypt(fileBytes, true);
+
+                    string encryptedFilePath = Path.Combine(Path.GetDirectoryName(txtFilePath.Text),
+                        Path.GetFileNameWithoutExtension(txtFilePath.Text) + "_encrypted" +
+                        Path.GetExtension(txtFilePath.Text));
+
+                    using (FileStream fs = new FileStream(encryptedFilePath, FileMode.Create))
+                    {
+                        fs.Write(encryptedBytes, 0, encryptedBytes.Length);
+                    }
+
+                    MessageBox.Show("Szyfrowanie przebiegło pomyślnie");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Encryption failed: " + ex.Message, "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz plik do zaszyfriwania.", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFilePath.Text))
+            {
+                try
+                {
+                    byte[] encryptedBytes = File.ReadAllBytes(txtFilePath.Text);
+                    // Deszyfrowanie
+                    byte[] decryptedBytes = rsaProvider.Decrypt(encryptedBytes, true);
+
+                    string decryptedFilePath = Path.Combine(Path.GetDirectoryName(txtFilePath.Text),
+                        Path.GetFileNameWithoutExtension(txtFilePath.Text) + "_decrypted" +
+                        Path.GetExtension(txtFilePath.Text));
+
+                    using (FileStream fs = new FileStream(decryptedFilePath, FileMode.Create))
+                    {
+                        fs.Write(decryptedBytes, 0, decryptedBytes.Length);
+                    }
+
+                    MessageBox.Show("Dzeszyfrowanie przebiegło pomyślnie");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd Deszyfriowania" + ex.Message, "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz plik do dezaszyfriwania.", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
